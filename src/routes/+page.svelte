ef4a5919d26a17d12b97e5a740e89f315204f290
satr14 <admin@satr14.my.id>
2026-07-27 18:53:50 +0700

    base layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,49 @@
 <script lang="ts">
-  
+  import { onMount } from "svelte";
+
+  let nameRolling = $state('your-name');
+  let counters = $state({
+    stars: 0,
+    forks: 0,
+    subdomains: 0,
+  });
+  const NAMES = [
+    "satr14",
+    "orangc",
+    "stefdp",
+    "iostpa",
+  ];
+
+  onMount(async () => {
+    const count = await fetch('https://raw.githubusercontent.com/partofmyid/register/refs/heads/main/stats/count.txt').then(r => r.text());
+    const repoStats = await fetch('https://api.github.com/repos/partofmyid/register').then(r => r.json());
+    const orgMembers = await fetch('https://api.github.com/orgs/partofmyid/members').then(r => r.json());
+
+    
+  });
 </script>
 
 <div class="h-full flex justify-center items-center">
-  <div>
-    <h1 class="text-4xl">coming soon</h1>
+  <div class="flex flex-col justify-center items-center gap-6">
+    <div>
+      <h1 class="text-center text-4xl font-bold">
+        <span class="text-ctp-mauve">{nameRolling}</span>.part-of.<span class="text-ctp-red">my.id</span>
+      </h1>
+      <p class="text-center">your own personal id for your website</p>
+    </div>
+    <div class="flex justify-center items-center gap-6">
+      <div class="flex flex-col justify-center items-center text-ctp-yellow">
+        <strong class="text-4xl">{counters.stars}</strong>
+        <span>stars</span>
+      </div>
+      <div class="flex flex-col justify-center items-center text-ctp-green">
+        <strong class="text-4xl">{counters.subdomains}</strong>
+        <span>subdomains</span>
+      </div>
+      <div class="flex flex-col justify-center items-center text-ctp-blue">
+        <strong class="text-4xl">{counters.forks}</strong>
+        <span>forks</span>
+      </div>
+    </div>
   </div>
 </div>
