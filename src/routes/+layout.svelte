04210ef7efc46ea1a0c39f55eb7460a9ac359f61
satr14 <admin@satr14.my.id>
2026-08-06 20:30:25 +0700

    spacing adjustment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<header class="max-w-3xl fixed top-0 left-0 right-0 flex justify-between px-4 py-2 mx-auto text-lg rounded-b-lg border-b border-x border-ctp-overlay0 bg-ctp-crust">
+<header class="max-w-3xl fixed top-0 left-0 right-0 flex justify-between px-4 pb-2 pt-1 mx-auto text-lg rounded-b-lg border-b border-x border-ctp-overlay0 bg-ctp-crust">
   <h1 class="font-bold"><a href="/">part-of.my.id</a></h1>
   <nav class="flex gap-4">
     <a href="https://github.com/partofmyid/register">repo</a>
