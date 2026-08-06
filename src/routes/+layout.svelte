531ddcba1063a333aca4588c5f99674e7ffe4e1e
satr14 <admin@satr14.my.id>
2026-08-06 20:12:25 +0700

    header and navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,14 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<header class="max-w-3xl fixed top-0 left-0 right-0 flex justify-between px-4 py-2 mx-auto text-lg rounded-b-lg border-b border-x border-ctp-overlay0 bg-ctp-crust">
+  <h1 class="font-bold"><a href="/">part-of.my.id</a></h1>
+  <nav class="flex gap-4">
+    <a href="https://github.com/partofmyid/register">repo</a>
+    <a href="/docs">docs</a>
+    <a href="/config">config</a>
+  </nav>
+</header>
+
 {@render children()}
