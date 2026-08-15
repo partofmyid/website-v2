d2889a9ecf73ca0ffe1f1fc2f434bde611dc984f
satr14 <admin@satr14.my.id>
2026-08-15 08:42:13 +0700

    base query page

---
+++ src/routes/q/[query]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import type { PageProps } from "./$types";
+  let { params }: PageProps = $props();
+
+  let input = $derived(params.query);
+
+</script>
+
+<div class="page-container">
+  <form onsubmit={(e) => { e.preventDefault(); goto(`/q/${input}`) }}>
+    <input type="text" bind:value={input}>
+    <button type="submit">search</button>
+  </form>
+  
+</div>
