b3ee5e0dd80b8c59fbd7a91fdf125b57f6e7d772
satr14 <admin@satr14.my.id>
2026-09-12 22:36:03 +0700

    create draft object for input handling and add description field

--- src/routes/record/+page.svelte
+++ src/routes/record/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { getSubdomain } from "$lib";
+  import { getSubdomain, type DomainFile } from "$lib";
   import { onMount } from "svelte";
 
   function constructURL(subdomain: string, apex: string, records: any = {}) {
@@ -14,11 +14,11 @@
   let apex = $derived($page.url.searchParams.get('apex') ?? '');
   let redirectURL = $derived(constructURL(subdomain, apex));
   let info: Awaited<ReturnType<typeof getSubdomain>> | null = $state(null);
-  let proxied = $state(false);
+  let draft: DomainFile = $state({ owner: { username: '' }, records: {} });
 
   onMount(async () => {
     info = await getSubdomain(subdomain, apex);
-    proxied = info?.data?.proxied ?? false;
+    if (info?.data) draft = { ...info.data };
   });
 </script>
 
@@ -26,7 +26,7 @@
   <h2 class="text-xl"><b>{subdomain}</b>.<i>{apex}</i></h2>
   {#if !info?.available && info?.data}
     <p>
-      {info.data.description || ''} - {info.data.owner.username}
+      - {info.data.owner.username}
       <img src="https://github.com/{info.data.owner.username}.png?size=32"
         alt="{info.data.owner.username}'s profile picture"
         class="h-6 aspect-square rounded-full outline-ctp-overlay0 outline inline">
@@ -36,11 +36,13 @@
   {:else}
     <p class="animate-pulse text-ctp-subtext0">Loading...</p>
   {/if}
-  <form>
+  <form class="flex flex-col gap-2 my-4">
+    <input type="text" bind:value={draft.description} placeholder="Description" class="w-full">
     <label>
-      <input type="checkbox" bind:checked={proxied}>
-      <img src="https://cdn.simpleicons.org/cloudflare/fab387" alt="Orange Clouding" class="inline h-6 {proxied ? "" : "grayscale"}">
-      {proxied ? "Proxied" : "DNS only"}
+      <input type="checkbox" bind:checked={draft.proxied}>
+      <img src="https://cdn.simpleicons.org/cloudflare/fab387" alt="Orange Clouding" class="inline h-6 {draft.proxied ? "" : "grayscale"}">
+      {draft.proxied ? "Proxied" : "DNS only"}
     </label>
+
   </form>
 </div>
