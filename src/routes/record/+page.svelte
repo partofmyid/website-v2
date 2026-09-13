7c2cbefb91a29d8c8f69819470c6d73aaf30d8b9
satr14 <admin@satr14.my.id>
2026-09-13 09:40:19 +0700

    username pfp preview and form for ARRAY_RECORDS

--- src/routes/record/+page.svelte
+++ src/routes/record/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { getSubdomain, type DomainFile } from "$lib";
+  import { ARRAY_RECORDS, getSubdomain, type DomainFile, type ArrayRecordType } from "$lib";
   import { onMount } from "svelte";
 
   function constructURL(subdomain: string, apex: string, records: any = {}) {
     return `https://github.com/partofmyid/register/new/main/domains/${apex}?${new URLSearchParams({
       filename: subdomain + '.json',
       value: JSON.stringify(records),
-    })}`
+    })}`;
+  }
+
+  function addRecord(type: ArrayRecordType) {
+    if (!draft.records[type]) draft.records[type] = [];
+    draft.records[type]!.push('');
   }
 
   let subdomain = $derived($page.url.searchParams.get('subdomain') ?? '');
@@ -15,10 +20,12 @@
   let redirectURL = $derived(constructURL(subdomain, apex));
   let info: Awaited<ReturnType<typeof getSubdomain>> | null = $state(null);
   let draft: DomainFile = $state({ owner: { username: '' }, records: {} });
+  let previewUsername = $state('');
 
   onMount(async () => {
     info = await getSubdomain(subdomain, apex);
     if (info?.data) draft = { ...info.data };
+    previewUsername = draft.owner.username;
   });
 </script>
 
@@ -27,7 +34,7 @@
   {#if !info?.available && info?.data}
     <p>
       - {info.data.owner.username}
-      <img src="https://github.com/{info.data.owner.username}.png?size=32"
+      <img src="https://github.com/{previewUsername}.png?size=32"
         alt="{info.data.owner.username}'s profile picture"
         class="h-6 aspect-square rounded-full outline-ctp-overlay0 outline inline">
     </p>
@@ -36,13 +43,36 @@
   {:else}
     <p class="animate-pulse text-ctp-subtext0">Loading...</p>
   {/if}
-  <form class="flex flex-col gap-2 my-4">
-    <input type="text" bind:value={draft.description} placeholder="Description" class="w-full">
+  <form class="flex flex-col gap-4 my-4" onsubmit={(e) => e.preventDefault()}>
+    <div>
+      {#if previewUsername}
+        <img src="https://github.com/{previewUsername}.png?size=32" alt=""
+          class="h-10 aspect-square outline-ctp-overlay0 outline inline">
+      {/if}
+      <input type="text" bind:value={draft.owner.username} onblur={() => previewUsername = draft.owner.username} placeholder="GitHub Username" class="w-[25%] inline" required>
+      <input type="text" bind:value={draft.description} placeholder="Subdomain Description" class="w-[65%] inline">
+    </div>
+    <hr>
+    <div class="flex gap-2 items-center">
+      <b class="text-xl">Add Record:</b>
+      {#each ARRAY_RECORDS as type}
+        <button class="bg-ctp-surface0 hover:bg-ctp-green hover:text-ctp-crust py-1 px-2"
+          onclick={(e) => { e.preventDefault(); addRecord(type) }}>{type}</button>
+      {/each}
+    </div>
+    {#each ARRAY_RECORDS as type}
+      {#each draft.records[type] ?? [] as _, i}
+        <div class="flex gap-2">
+          <input type="text" bind:value={draft.records[type]![i]} placeholder="{type} Record #{i+1}" class="inline flex-1">
+          <button class="bg-ctp-surface0 text-ctp-red hover:bg-ctp-red hover:text-ctp-crust py-1 px-2"
+            onclick={(e) => { e.preventDefault(); draft.records[type]?.splice(i,1) }}>Remove</button>
+        </div>
+      {/each}
+    {/each}
     <label>
       <input type="checkbox" bind:checked={draft.proxied}>
       <img src="https://cdn.simpleicons.org/cloudflare/fab387" alt="Orange Clouding" class="inline h-6 {draft.proxied ? "" : "grayscale"}">
       {draft.proxied ? "Proxied" : "DNS only"}
     </label>
-
   </form>
 </div>
