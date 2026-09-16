372c1f522c6c4c8f8199364701a24a47842c32b4
satr14 <admin@satr14.my.id>
2026-09-16 22:05:40 +0700

    fix z-index header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 </svelte:head>
 
 <div class="min-h-full flex flex-col">
-  <header class="max-w-3xl fixed top-0 left-0 right-0 flex justify-between px-4 pb-2 pt-1 mx-6 md:mx-auto text-lg rounded-b-lg border-b border-x border-ctp-overlay0 bg-ctp-crust">
+  <header class="max-w-3xl fixed top-0 left-0 right-0 flex justify-between px-4 pb-2 pt-1 mx-6 md:mx-auto text-lg rounded-b-lg border-b border-x border-ctp-overlay0 bg-ctp-crust z-10">
     <h1 class="font-bold text-ctp-lavender"><a href="/">part-of.my.id</a></h1>
     <nav class="flex gap-4">
       <a href="https://github.com/partofmyid/register">repo</a>
